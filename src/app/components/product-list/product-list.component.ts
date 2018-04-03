cf6f196bdf5cc99320cc6079bfd72e69558d39c8
Achraf MABROUK <achraf.mabrouk3@gmail.com>
2018-04-03 11:25:17 +0200

    Ajout d'un nouveau produit

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -14,7 +14,8 @@ export class ProductListComponent implements OnInit {
       { code: 'P100', libelle: 'Cafe', prixUnitaire: 500.5 },
       { code: 'P200', libelle: 'Thé', prixUnitaire: 500.5 },
       { code: 'P300', libelle: 'Jus d\'orange', prixUnitaire: 500.5 },
-      { code: 'P400', libelle: 'Coca Cola', prixUnitaire: 500.5 }
+      { code: 'P400', libelle: 'Coca Cola', prixUnitaire: 500.5 },
+      { code: 'P500', libelle: 'Eau gazifiee', prixUnitaire: 500.5 }
     ];
   }
 
