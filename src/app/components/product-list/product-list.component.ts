cc5191c3e5c711344e829a335f5e245e4fc2879a
anouar <dsi.anouar.abid@gmail.com>
2018-04-03 11:17:28 +0200

    commit initial

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../domain/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  data: Product[];
+
+  constructor() {
+    this.data = [
+      { code: 'P100', libelle: 'Cafe', prixUnitaire: 500.5 },
+      { code: 'P200', libelle: 'Thé', prixUnitaire: 500.5 },
+      { code: 'P300', libelle: 'Jus d\'orange', prixUnitaire: 500.5 },
+      { code: 'P400', libelle: 'Coca Cola', prixUnitaire: 500.5 }
+    ];
+  }
+
+  ngOnInit() {}
+}
