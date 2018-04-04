7f09515a898fdb3d18bbe1a7198653fd08ec9864
Achraf MABROUK <achraf.achref@sagemcom.com>
2018-04-04 14:17:42 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -8,18 +8,30 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductService } from './services/product.service';
 import { HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
+const routes = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', component: WelcomeComponent, pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductListComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
